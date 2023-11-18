ee71a2099f559ebc03eb7ea74a937065a268611f
Matei Ivan Tudor <m.t.ivan-1@student.tudelft.nl>
2023-11-18 23:31:38 +0100

    Data is written to file when save command is used, and that file is read every time you start the bot process all over, so you don't need to add again tokens or other data the user might've inputted

--- src/server.py
+++ src/server.py
@@ -6,7 +6,9 @@ from flask import Flask, request
 from miscellaneous import prettify_json
 from telegram_bot import TelegramBot
 import json
+
 app: Flask = Flask(__name__)
+
 class Server(Thread):
     def __init__(self):
         super().__init__()
@@ -29,10 +31,18 @@ class Server(Thread):
     async def branch_create():
         bot: TelegramBot = TelegramBot().get_instance()
         file = open('github_update.txt', 'w')
-        file.write(prettify_json(json.dumps(request.json)))
+        js = json.dumps(request.json)
+        file.write(prettify_json(js))
         file.close()
 
-        asyncio.run_coroutine_threadsafe(bot.send_update('A new branch has been created!'), bot.get_event_loop())
+        data = json.loads(js)
+        sender = data['sender']['login']
+        branch_name = data['ref']
+
+        # await asyncio.sleep(0.1)
+
+        asyncio.run_coroutine_threadsafe(bot.send_update(f'User {sender} has created a new branch {branch_name}!'),
+                                         bot.get_event_loop())
 
         return 'Success', 200
 
@@ -41,9 +51,16 @@ class Server(Thread):
     async def issue():
         bot: TelegramBot = TelegramBot().get_instance()
         file = open('issue_updates.txt', 'w')
-        file.write(prettify_json(json.dumps(request.json)))
+        js = json.dumps(request.json)
+        file.write(prettify_json(js))
         file.close()
 
-        asyncio.run_coroutine_threadsafe(bot.send_update('Something happened with the issues'), bot.get_event_loop())
+        data = json.loads(js)
+        sender = data['sender']['login']
+        action = data['action']
+        title = data['issue']['title']
+        number = data['issue']['number']
+
+        asyncio.run_coroutine_threadsafe(bot.send_update(f'User {sender} has {action} issue #{number} "{title}"'), bot.get_event_loop())
 
         return 'Success', 200
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from typing import Final
 from telegram import Update
 from telegram.ext import ContextTypes
@@ -22,7 +23,7 @@ class TelegramBot(Thread):
         self.app: Application = Application.builder().token(TelegramBot.TOKEN).build()
         self.chat_id = chat_id
         self.gh_token = gh_token
-        self.github_account: Github | None = Github(gh_token) if gh_token is not None else None
+        # self.github_account: Github | None = Github(gh_token) if gh_token is not None else None
         self.lock = Lock()
         # self._initialized = True
 
@@ -63,7 +64,7 @@ class TelegramBot(Thread):
     async def link_github_token_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         account_token = ' '.join(context.args)
         self.gh_token = account_token
-        self.github_account = Github(account_token)
+        # self.github_account = Github(account_token)
         await update.message.reply_text('GitHub account token updated')
 
     async def link_github_repo_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -77,19 +78,24 @@ class TelegramBot(Thread):
         await update.message.reply_text('Name updated')
 
     async def save_to_file(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        with open('shared_file.txt', 'w') as file:
-            file.write(f"{self.gh_token}\n{self.repo}\n{self.username}\n{self.chat_id}")
+        with open('user_data.txt', 'w') as file:
+            file.write(f"{self.chat_id}\n{self.username}\n{self.repo}\n{self.gh_token}")
         await update.message.reply_text('Saved GitHub data to file')
 
     async def create_issue(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        print("HERE")
         with self.lock:
             title = context.args[0]
             body = context.args[1]
-            issue = self.github_account.get_user(self.username).get_repo(self.repo).create_issue(
+            gh = Github(self.gh_token).get_user(self.username).get_repo(self.repo)
+            issue = gh.create_issue(
                 title=title,
                 body=body
             )
+
             await update.message.reply_text(f'Issue created at {issue.html_url}')
+
+
 
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         message_type: str = update.message.chat.type  # tells whether group chat or private conversation
@@ -127,6 +133,19 @@ class TelegramBot(Thread):
 
         # Error
         self.app.add_error_handler(self.error)
+
+        print(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'user_data.txt'))
+
+        if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'user_data.txt')):
+            print("HERE")
+            with open('user_data.txt', 'r') as file:
+                tokens = file.read().split('\n')
+                self.chat_id = tokens[0]
+                self.username = tokens[1]
+                self.repo = tokens[2]
+                self.gh_token = tokens[3]
+
+        print(self.chat_id, self.username, self.repo, self.gh_token)
 
         TelegramBot._instance = self
 
